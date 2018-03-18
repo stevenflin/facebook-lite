93aba61c32dfd5905c8d8bb0122c7bd9ddcbffe9
stevenflin <steven.lin@stern.nyu.edu>
2018-03-18 12:48:53 -0400

    make custom chip

--- frontend/src/components/NewsfeedPage/Newsfeed/Comment.js
+++ frontend/src/components/NewsfeedPage/Newsfeed/Comment.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
-import Chip from 'material-ui/Chip';
-
 const Comment = ({comment}) => (
 	<div className='comment'>
-		<Chip style={{backgroundColor: 'white'}}>	
 			<p className='reset-spacing fss'><strong>{`${comment.firstName} ${comment.lastName}`}</strong> {comment.content}</p>
-		</Chip>
 	</div>
 );
 
